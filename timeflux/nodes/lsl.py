Fix mismatch management of units in sync vs lsl

timeflux/nodes/lsl.py
@@ -107,7 +107,7 @@ class Receive(Node):
         self._channels = channels
         self._timeout = timeout
         self._max_samples = max_samples
-        self._offset = time() - pylsl.local_clock()
+        self._offset = np.timedelta64(time() - pylsl.local_clock(), 's')
 
     def update(self):
         if not self._inlet:
@@ -135,10 +135,9 @@ class Receive(Node):
         if self._inlet:
             values, stamps = self._inlet.pull_chunk(max_samples=self._max_samples)
             if stamps:
-                stamps = np.array(stamps)
+                stamps = pd.to_datetime(stamps, format=None, unit=self._unit)
                 if self._sync == 'local':
                     stamps += self._offset
                 elif self._sync == 'network':
-                    stamps = stamps + self._inlet.time_correction() + self._offset
-                stamps = pd.to_datetime(stamps, format=None, unit=self._unit)
+                    stamps = stamps + np.timedelta64(self._inlet.time_correction(), 's') + self._offset
             self.o.set(values, stamps, self._labels, self._meta)